67173731a613b56e25b42c917adbaf8c28c584a1
Patrick <patrick.bryant@live.com>
2020-06-09 12:52:49 +0200

    making sure transpose is correct

--- src/mutual_information/mi.py
+++ src/mutual_information/mi.py
@@ -142,7 +142,7 @@ name_pair = names[0]+'_'+names[1]
 #Read in msa
 a3m_matrix = read_a3m(infile)
 #Calculate mututal information
-mi_matrix = calc_mi(a3m_matrix, name_pair, outdir)
-#mi_matrix = np.load('O13297_Q01159.npy', allow_pickle=True)
+#mi_matrix = calc_mi(a3m_matrix, name_pair, outdir)
+mi_matrix = np.load(outdir+'O13297_Q01159.npy', allow_pickle=True)
 #Perform APC
 APC(mi_matrix, l1, l2, name_pair, outdir)
