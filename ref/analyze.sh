3d1c646d885e921062eae857174497c84b27be0c
Patrick <patrick.bryant@live.com>
2020-07-08 16:50:37 +0200

    mapping ids

--- ref/analyze.sh
+++ ref/analyze.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 IP=./Y2H_union.txt
 NF5=./nf5.txt
+ID_CONV=./gene_id_meta_uniprot.tab
 OUTDIR=./
-./analyze_bench.py --interacting_pairs $IP --nf5_pairs $NF5 --outdir $OUTDIR
+./analyze_bench.py --interacting_pairs $IP --nf5_pairs $NF5 --id_conversion $ID_CONV --outdir $OUTDIR
--- ref/analyze_bench.py
+++ ref/analyze_bench.py
@@ -20,21 +20,56 @@ parser = argparse.ArgumentParser(description = '''Visuaize results from model us
 
 parser.add_argument('--interacting_pairs', nargs=1, type= str, default=sys.stdin, help = 'Path to interacting pairs (Y2H_union).')
 parser.add_argument('--nf5_pairs', nargs=1, type= str, default=sys.stdin, help = 'Path to pairs in paired alignments with Nf5.')
+parser.add_argument('--id_conversion', nargs=1, type= str, default=sys.stdin, help = 'Tab delimited file with ID conversion.')
 parser.add_argument('--outdir', nargs=1, type= str, default=sys.stdin, help = 'Path to outdir.')
+
+def format_id_conversion(id_conversion):
+    '''Split all gene names and relate to protein id
+    '''
+    all_conversion = {}
+    for i in range(len(id_conversion)):
+        row = id_conversion.loc[i]
+        protein = row['Entry']
+        gene_names = row['Gene names']
+        for name in gene_names.split():
+            all_conversion[name]=protein
+
+    return all_conversion
+
+
+def covert_gene_to_protein_id(interacting_ids,id_conversion):
+    '''Convert the gene ids to protein ids
+    '''
+    converted_ids = []
+    for id in interacting_ids:
+        try:
+            converted_ids.append(id_conversion[id])
+        except:
+            converted_ids.append('missing')
+            print(id)
+    print('Fraction successfully converted:',len(converted_ids)/len(interacting_ids))
+
+    return converted_ids
 
 def read_interacting(interacting_id1, interacting_id2, all_ids_in_interactions, nf5_pairs):
     '''Read all pairs with n5 above or equal to 5
     '''
     true_pairs_in_n5 = []
     fetched_pairs = [] #Fetch the pairs that have one of the ids in the true interacting pairs
-    with open(nf5_pairs, 'r') as file:
-        for line in file:
-            line = line.split()
-            id1 = line[0]
-            id2 = line[1]
-            if id1 in
+    for id1 in interacting_id1:
+        print(id1)
+        if id1 in np.array(nf5_pairs[0]):
+            pdb.set_trace()
+        if id1 in np.array(nf5_pairs[1]):
+            pdb.set_trace()
+    pdb.set_trace()
+    for id2 in interacting_id2:
+        if id2 in np.array(nf5_pairs[0]):
+            pdb.set_trace()
+        if id2 in np.array(nf5_pairs[1]):
             pdb.set_trace()
 
+    pdb.set_trace()
 #####MAIN#####
 #Set font size
 matplotlib.rcParams.update({'font.size': 7})
@@ -42,8 +77,16 @@ args = parser.parse_args()
 interacting_pairs = pd.read_csv(args.interacting_pairs[0], sep = '\t', header = None)
 interacting_id1= np.array(interacting_pairs[0])
 interacting_id2= np.array(interacting_pairs[1])
-all_ids_in_interactions =  np.concatenate([interacting_id1,interacting_id2])
-nf5_pairs = args.nf5_pairs[0]
+all_ids_in_interactions =  np.unique(np.concatenate([interacting_id1,interacting_id2]))
+nf5_pairs = pd.read_csv(args.nf5_pairs[0], sep = ' ', header = None)
+id_conversion = pd.read_csv(args.id_conversion[0],sep = '\t')
 outdir = args.outdir[0]
 
-read_interacting(interacting_id1, interacting_id2, all_ids_in_interactions, nf5_pairs)
+#Get all gene names for the id conversion
+id_conversion = format_id_conversion(id_conversion)
+#Convert all gene ids to protein ids
+converted_ids1 = covert_gene_to_protein_id(interacting_id1,id_conversion)
+converted_ids2 = covert_gene_to_protein_id(interacting_id2,id_conversion)
+pdb.set_trace()
+#Map joined msa ids to positive set
+read_interacting(id_conversion, interacting_id1, interacting_id2, all_ids_in_interactions, nf5_pairs)
